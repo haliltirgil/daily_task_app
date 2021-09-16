f7c5b2fe1bdb31a85e3fa0d2402dd20e7c5e6b55
haliltirgil <tirgil18@gmail.com>
2021-09-16 08:38:50 +0300

    Add new dropdown button for choose job detail

--- lib/widgets/dropdown_category.dart
+++ lib/widgets/dropdown_category.dart
@@ -21,6 +21,26 @@ class _DropdownCategoryState extends State<DropdownCategory> {
   }
 
   Widget _buildJobDetailDropdown(String text) {
+    String value = text;
+
+    var items = [
+      'SVT Projesi',
+      'Banana',
+      'Grapes',
+      'Orange',
+      'İzin',
+      'Pineapple'
+    ];
+
+    var items1 = [
+      'İzin',
+      'Ver',
+      'Calısmak',
+      'Sıktı',
+      'Admin',
+      'Kardes',
+    ];
+
     return Container(
       width: MediaQuery.of(context).size.width / 2,
       height: MediaQuery.of(context).size.height / 15,
@@ -30,9 +50,35 @@ class _DropdownCategoryState extends State<DropdownCategory> {
           const Radius.circular(12.0),
         ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Text(text),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton(
+          value: value,
+          icon: Icon(Icons.keyboard_arrow_down),
+          items: value == 'SVT Projesi'
+              ? items.map((String items) {
+                  return DropdownMenuItem(
+                      value: items,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            left: MediaQuery.of(context).size.width / 40),
+                        child: Text(items),
+                      ));
+                }).toList()
+              : items1.map((String items) {
+                  return DropdownMenuItem(
+                      value: items,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            left: MediaQuery.of(context).size.width / 40),
+                        child: Text(items),
+                      ));
+                }).toList(),
+          onChanged: (String newValue) {
+            setState(() {
+              value = newValue;
+            });
+          },
+        ),
       ),
     );
   }
